b95feec08d86c717d7b798ca6be4260d5b26840c
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-06 23:48:16 +0530

    Document the Platform.kt class

--- library/src/commonMain/kotlin/io/github/sudarshanmhasrup/hellokmp/Platform.kt
+++ library/src/commonMain/kotlin/io/github/sudarshanmhasrup/hellokmp/Platform.kt
@@ -1,6 +1,12 @@
 package io.github.sudarshanmhasrup.hellokmp
 
+/**
+ * Represents the current platform (Android, iOS, Desktop, Web, etc.).
+ */
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 public expect class Platform {
+    /**
+     * Returns the name of the current platform.
+     */
     public fun getPlatformName(): String
 }
