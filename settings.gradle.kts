bb117f74ea9a581e001e1b008cadd50e9ef99ada
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-06 23:07:40 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+rootProject.name = "HelloKmp"
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}
