65b317a7f23c72b1354f0f0ff37cfc0d7914636a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-06 23:46:09 +0530

    Make the declaration of Platform.getPlatformName() public

--- library/src/commonMain/kotlin/io/github/sudarshanmhasrup/hellokmp/Platform.kt
+++ library/src/commonMain/kotlin/io/github/sudarshanmhasrup/hellokmp/Platform.kt
@@ -2,5 +2,5 @@ package io.github.sudarshanmhasrup.hellokmp
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 public expect class Platform {
-    fun getPlatformName(): String
+    public fun getPlatformName(): String
 }
