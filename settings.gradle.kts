a05a6ef9e0f5eb7ffcf4addee1d104b06fdff943
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-07 21:04:03 +0530

    Add the google() as location for plugin management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,15 @@
 rootProject.name = "HelloKmp"
 
+@Suppress("UnstableApiUsage")
 pluginManagement {
     repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
