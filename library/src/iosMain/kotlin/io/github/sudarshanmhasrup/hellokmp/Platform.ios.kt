627a720677c5b116f9fc106c115cb3a4bb58a176
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-07 20:51:04 +0530

    Provide the actual implementation for the iOS platform

---
+++ library/src/iosMain/kotlin/io/github/sudarshanmhasrup/hellokmp/Platform.ios.kt
@@ -0,0 +1,10 @@
+package io.github.sudarshanmhasrup.hellokmp
+
+import platform.UIKit.UIDevice
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING", "RedundantVisibilityModifier")
+public actual class Platform {
+    actual fun getPlatformName(): String {
+        return UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+    }
+}
