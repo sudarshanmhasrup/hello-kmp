f527904e61e8955a79a20471b64085ac6a9329f5
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-07 20:50:29 +0530

    Remove the redundant suppress value

--- library/src/commonMain/kotlin/io/github/sudarshanmhasrup/hellokmp/Platform.kt
+++ library/src/commonMain/kotlin/io/github/sudarshanmhasrup/hellokmp/Platform.kt
@@ -3,7 +3,7 @@ package io.github.sudarshanmhasrup.hellokmp
 /**
  * Represents the current platform (Android, iOS, Desktop, Web, etc.).
  */
-@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING", "unused", "RedundantVisibilityModifier")
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING", "RedundantVisibilityModifier")
 public expect class Platform {
     /**
      * Returns the name of the current platform.
