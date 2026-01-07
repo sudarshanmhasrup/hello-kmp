ef8642450406b6763f81007362905e9a6b649b99
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-07 21:39:14 +0530

    Configure Maven publishing for the library

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -31,3 +31,40 @@ kotlin {
         binaries.executable()
     }
 }
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates(
+        groupId = libs.versions.library.group.get(),
+        artifactId = libs.versions.library.artifactId.get(),
+        version = libs.versions.library.version.get()
+    )
+
+    pom {
+        name.set("HelloKMP")
+        description.set("A sample Kotlin Multiplatform library template that you can use to structure your next Kotlin Multiplatform library.")
+        inceptionYear.set("2026")
+        url.set("https://github.com/sudarshanmhasrup/hello-kmp")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("repo")
+            }
+        }
+        developers {
+            developer {
+                id.set("sudarshanmhasrup")
+                name.set("Sudarshan")
+                url.set("https://github.com/sudarshanmhasrup")
+            }
+        }
+        scm {
+            url.set("https://github.com/sudarshanmhasrup/hello-kmp")
+            connection.set("scm:git:https://github.com/sudarshanmhasrup/hello-kmp.git")
+            developerConnection.set("scm:git:ssh://git@github.com/sudarshanmhasrup/hello-kmp.git")
+        }
+    }
+}
