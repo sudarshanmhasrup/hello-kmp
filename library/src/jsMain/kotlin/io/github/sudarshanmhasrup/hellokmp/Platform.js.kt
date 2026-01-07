6deeb921033f66b4a90e9ea61a56107d6e9a7f8e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-07 20:55:07 +0530

    Provide the actual implementation for the JS platform

---
+++ library/src/jsMain/kotlin/io/github/sudarshanmhasrup/hellokmp/Platform.js.kt
@@ -0,0 +1,8 @@
+package io.github.sudarshanmhasrup.hellokmp
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING", "RedundantVisibilityModifier")
+public actual class Platform {
+    actual fun getPlatformName(): String {
+        return "Web with Kotlin/JS"
+    }
+}
