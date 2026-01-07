b0ea7b1222405ad2651dd2b22b480db444f0854e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-07 21:35:09 +0530

    Apply the "com.vanniktech.maven.publish" plugin to the library module

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.maven.publish)
 }
 
 group = libs.versions.library.group.get()
