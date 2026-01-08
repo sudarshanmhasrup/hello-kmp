ad1743ab909991bd990614781e4a88584868452d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-08 15:50:49 +0530

    Configure the shared module for Kotlin Multiplatform env

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
@@ -6,6 +8,32 @@ plugins {
 group = libs.versions.shared.group.get()
 version = libs.versions.shared.version.get()
 
+kotlin {
+    androidLibrary {
+        namespace = libs.versions.library.androidLibrary.namespace.get()
+        compileSdk = libs.versions.library.androidLibrary.compileSdk.get().toInt()
+        minSdk = libs.versions.library.androidLibrary.minSdk.get().toInt()
+
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
+    }
+
+    jvm("desktop") {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
+    }
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    js {
+        browser()
+        binaries.executable()
+    }
+}
 
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/shared"))
