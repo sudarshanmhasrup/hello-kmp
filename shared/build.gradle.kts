d93baf6b7d17279b4a8a10f1a01b309ca1b8361d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-09 00:20:34 +0530

    Apply the "org.jetbrains.compose" plugin to the shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.compose.multiplatform)
 }
 
 group = libs.versions.shared.group.get()
