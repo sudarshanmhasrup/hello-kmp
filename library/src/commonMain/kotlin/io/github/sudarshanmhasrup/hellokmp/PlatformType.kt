74ba1aab644096e679dae97d38f53848662255d7
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-09 00:03:54 +0530

    Create the "PlatformType" enum to represent supported platforms

---
+++ library/src/commonMain/kotlin/io/github/sudarshanmhasrup/hellokmp/PlatformType.kt
@@ -0,0 +1,13 @@
+package io.github.sudarshanmhasrup.hellokmp
+
+/**
+ * Represents supported platform types.
+ */
+@Suppress("RedundantVisibilityModifier")
+public enum class PlatformType {
+    Android,
+    Ios,
+    Desktop,
+    JS,
+    Wasm
+}
