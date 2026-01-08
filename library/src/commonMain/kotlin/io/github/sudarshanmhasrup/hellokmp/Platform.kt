4b1b48e0dbb73bcbbf9bdd6a9a3fadc90a853f9a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-09 00:03:02 +0530

    Remove "Web" from the KDoc for the Platform class

--- library/src/commonMain/kotlin/io/github/sudarshanmhasrup/hellokmp/Platform.kt
+++ library/src/commonMain/kotlin/io/github/sudarshanmhasrup/hellokmp/Platform.kt
@@ -1,7 +1,7 @@
 package io.github.sudarshanmhasrup.hellokmp
 
 /**
- * Represents the current platform (Android, iOS, Desktop, Web, etc.).
+ * Represents the current platform (Android, iOS, Desktop, etc.).
  */
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING", "RedundantVisibilityModifier")
 public expect class Platform() {
