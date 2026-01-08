d406a7686b0e621ac01701d9a89c43b1d4a9c03e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-08 10:10:36 +0530

    Add the Foojay resolver convention plugin to Gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
