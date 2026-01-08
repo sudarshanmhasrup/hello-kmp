f8c6ce4321e84408dfcf7d06bef4e6bb3ca34064
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-08 15:49:02 +0530

    Apply the "com.android.kotlin.multiplatform.library" plugin to the shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
 }
 
 group = libs.versions.shared.group.get()
 version = libs.versions.shared.version.get()
+
 
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/shared"))
