37c9b0ed210253893082d1ccc1bf793b9e75bda0
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-08 15:45:57 +0530

    Define the shared module's configuration information from the version catalog

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,5 +2,8 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
 }
 
+group = libs.versions.shared.group.get()
+version = libs.versions.shared.version.get()
+
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/shared"))
