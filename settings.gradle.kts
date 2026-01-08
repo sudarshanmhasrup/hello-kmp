fd66101b91fbaa97dd961084ca50202e11575355
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-08 16:18:30 +0530

    Add Google Maven repository to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,13 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
     }
 }
