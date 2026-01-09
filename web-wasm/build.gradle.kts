c94641c618024f63c382350db01024a2cafadc14
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-09 21:00:07 +0530

    Apply the "org.jetbrains.kotlin.plugin.compose" plugin to teh web-wasm module

--- web-wasm/build.gradle.kts
+++ web-wasm/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
 }
 
 group = libs.versions.webWasm.group.get()
