80701dedbc5c4e9e314354ab936c72dc139602de
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-01-09 15:55:11 +0530

    Define the web-wasm module's configuration information from the version catalog

--- web-wasm/build.gradle.kts
+++ web-wasm/build.gradle.kts
@@ -2,5 +2,8 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
 }
 
+group = libs.versions.webWasm.group.get()
+version = libs.versions.webWasm.version.get()
+
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/web-wasm"))
